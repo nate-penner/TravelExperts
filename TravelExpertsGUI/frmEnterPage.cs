570e120a185b9112f36089de0098abfc6aa45a5e
Daniel Palmer <daniel.palmer@shaw.ca>
2022-01-27 01:51:32 -0700

    Started work on entrance splash screen

---
+++ TravelExpertsGUI/frmEnterPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelExpertsGUI
+{
+    public partial class frmEnterPage : Form
+    {
+        public frmEnterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
